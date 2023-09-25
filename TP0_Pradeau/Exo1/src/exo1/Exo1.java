05989e838e2972b7a7adb2bba0cc213058517e5f
Audric-Antonin Pradeau <audricantonin.pradeau@epfedu.fr>
2023-09-25 17:49:09 +0200

    exo1

---
+++ TP0_Pradeau/Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ * TP0 Exercice 1
+   Pradeau Audric TDA
+  25/09/23
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("au revoir");
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, ca va ?");
+prenom = sc.nextLine();
+    }
+    
+}
