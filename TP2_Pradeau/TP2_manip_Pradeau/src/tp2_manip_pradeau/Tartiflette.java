1b9e85f6a2b915a63c7d2123b7365326787d4f11
Audric-Antonin Pradeau <audricantonin.pradeau@epfedu.fr>
2023-10-11 12:11:39 +0200

    TP1 Partie 4: stat

---
+++ TP2_Pradeau/TP2_manip_Pradeau/src/tp2_manip_pradeau/Tartiflette.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_pradeau;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Tartiflette {
+    int nbCalories;
+
+    Tartiflette(int calories) {
+        this.nbCalories = calories;
+    }
+}   
+
