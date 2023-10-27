eef4bbe7036d6460a5bc6975703a337525156434
Audric-Antonin Pradeau <audricantonin.pradeau@epfedu.fr>
2023-10-27 17:56:25 +0200

    Mini Projet numéro 1

--- Petit Projet 1/LightOff_Pradeau_version_console/src/lightoff_pradeau_version_console/LightOff_Pradeau_version_console.java
+++ Petit Projet 1/LightOff_Pradeau_version_console/src/lightoff_pradeau_version_console/LightOff_Pradeau_version_console.java
@@ -11,12 +11,14 @@ package lightoff_pradeau_version_console;
 public class LightOff_Pradeau_version_console {
 
     /**
-     * @param args the command line arguments
+     * La méthode principale du jeu LightOff en mode console. Elle crée une instance de la classe Partie
+     * et lance le jeu.
+     *
+     * @param args Les arguments de ligne de commande (non utilisés dans cette version du jeu).
      */
     public static void main(String[] args) {
         Partie partie = new Partie();
         partie.lancerPartie();
     }
-}
-    
+}    
 
